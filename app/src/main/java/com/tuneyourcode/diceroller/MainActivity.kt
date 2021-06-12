f91f38525481821643107b208a80c385c554c7f4
VARUN KUMAR <varunjha089@gmail.com>
2021-06-13 01:35:20 +0530

    Added ImageView to work with images of dice based random number genetated by code.

--- app/src/main/java/com/tuneyourcode/diceroller/MainActivity.kt
+++ app/src/main/java/com/tuneyourcode/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tuneyourcode.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.Random
@@ -12,23 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // This is how you can interact with
-//        val textView: TextView = findViewById(R.id.result_text);
-//        textView.text = "this is new text"
-
         val rollButton: Button = findViewById(R.id.roll_button);
         // https://developer.android.com/reference/android/view/View.OnClickListener
         rollButton.setOnClickListener{
             // https://developer.android.com/guide/topics/ui/notifiers/toasts
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text);
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
         val randomInt = Random().nextInt(6) + 1
 
-        resultText.text = randomInt.toString();
+        val drawableResources = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResources)
     }
 }
