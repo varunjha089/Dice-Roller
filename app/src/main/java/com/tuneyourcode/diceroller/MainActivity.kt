0b6d37275b78704841878e4441e35e7cdfe1f0e9
VARUN KUMAR <varunjha089@gmail.com>
2021-06-13 02:24:48 +0530

    taking steps to save the memory consumption.

--- app/src/main/java/com/tuneyourcode/diceroller/MainActivity.kt
+++ app/src/main/java/com/tuneyourcode/diceroller/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.tuneyourcode.diceroller
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,11 +22,11 @@ class MainActivity : AppCompatActivity() {
 //            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-
         val randomInt = Random().nextInt(6) + 1
 
         val drawableResources = when (randomInt) {
